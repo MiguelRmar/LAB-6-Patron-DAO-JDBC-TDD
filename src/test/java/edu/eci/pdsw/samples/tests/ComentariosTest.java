672f6cf84575fc5364c3b0cc4ac407d44241aad0
David Reina <david.reina@mail.escuelaing.edu.co>
2016-09-22 13:03:51 -0500

    version pruebas de entrada comentario

--- src/test/java/edu/eci/pdsw/samples/tests/ComentariosTest.java
+++ src/test/java/edu/eci/pdsw/samples/tests/ComentariosTest.java
@@ -16,6 +16,11 @@
  */
 package edu.eci.pdsw.samples.tests;
 
+import com.sun.xml.internal.ws.wsdl.writer.UsingAddressing;
+import edu.eci.pdsw.samples.entities.Comentario;
+import edu.eci.pdsw.samples.entities.Usuario;
+import java.sql.Date;
+import static jdk.nashorn.internal.objects.NativeRegExp.test;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -27,15 +32,42 @@ import static org.junit.Assert.*;
 public class ComentariosTest {
     
     public ComentariosTest() {
+
+/*
+        CLASES DE EQUIVALENCIA:
+        
+        CLASE DE EQUIVALENCIA1 : CUANDO VAMOS A VERIFICAR QUE AL MOMENTO DE REGISTRAR UNA RESPUESTA 
+        EL USUARIO EXISTA
+        TIPO: NORMAL
+        SE ESPERE UN ERROR: SI() NO(X)
+        
+        CLASE DE EQUIVALENCIA 2: CUANDO AL MOMENTO DE ESCRIBIR LA RESPUESTA AL FORO  INSTRODUCIMOS UNA FECHA MENOR 
+        A LA CUAL ESTA EL FORO CREADO
+        TIPO: DE FRONTERA
+        SE ESPERA UN ERROR: SI(X) NO(X)
+        
+        CLASE DE EQUIVALENCIA 3: CUANDO AL MOMENTO DE REGISTRAR LA RESPUESTA AL FORO  INSTRODUCIMOS UN COMENTARIO ERRONEO
+        TIPO: NORMAL
+        SE ESPERA UN ERROR: SI() NO(X)
+        
+        */
     }
     
     @Before
     public void setUp() {
     }
     
+
+    
     @Test
-    public void registroPacienteTest(){
-        
+    public void NuevaRespuestaTest(){
+        Usuario us = new Usuario("miguel@gmail.com","UsuarioX");
+        Date fecha = new Date(2016,10 ,30);
+        Comentario Com = new Comentario(us,"el foro esta muy interesante ",fecha);
+        String Contenido = Com.getContenido();
+        assertEquals("no logro generar el comentario", Contenido); 
+       
+                
     }
     
     
